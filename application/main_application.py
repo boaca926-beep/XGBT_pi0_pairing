9e52e64273c907f63f55a62e8dbf9fde00437108
boaca926 <bo.cao@physics.uu.se>
2026-03-16 19:02:20 +0100

    all changes

--- application/main_application.py
+++ application/main_application.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     input_data_dir = os.path.join(project_root, f'analysis/dataset')
     input_model_dir = os.path.join(project_root, f'training/models')
 
-    category_type = 'TCOMB' #'TETAGAM', 'TISR3PI_SIG', 'TKSL'
+    category_type = 'TETAGAM' #'TCOMB' #'TETAGAM', 'TISR3PI_SIG', 'TKSL'
 
     phys_map = joblib.load(os.path.join(input_data_dir, f'phys_map.pkl'))
     print(phys_map)
